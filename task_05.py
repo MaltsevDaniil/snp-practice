9c78c2417970af8310bc27e27852cdbc4e39b446
MaltsevDaniil <145263244+MaltsevDaniil@users.noreply.github.com>
2024-03-12 20:06:16 +0300

    Update task_05.py

--- task_05.py
+++ task_05.py
@@ -7,6 +7,3 @@ def date_in_future(integer=0):
         return (datetime.now() + timedelta(days=integer)).strftime('%d-%m-%Y %H:%M:%S')
     else:
         return datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-
-
-print(date_in_future())
